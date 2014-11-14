28434499e06a0587583051b66a2f3f379add4bbd
concreted <arichuang@gmail.com>
2014-11-13 19:37:27 -0800

    Attempted using directive, not working due to securl error loading templateUrl

--- chrome/app.js
+++ chrome/app.js
@@ -4,22 +4,12 @@ angular.module('yahooTest', [])
 
 .controller('yahooTestController', function($scope) {
   $scope.text = 'hello from angular';
-
-  // Listen for messages from content.js
-  window.addEventListener("message", receiveMessage, false);
-  function receiveMessage(event) {
-    console.log("message received!");
-    console.log(event.data);
-    $scope.text = event.data.data;
-    console.log($scope.text);
-    $scope.$apply();
-  }
 })
 
 .directive('yahoo', function() {
   var url = chrome.extension.getURL('app.html');
   return {
-    //templateUrl: url
-    template: '{{text}}'
+    templateUrl: url
+    //template: '{{text}}'
   };
 });
--- chrome/content.js
+++ chrome/content.js
@@ -1,27 +1,27 @@
 console.log('content.js');
 
-// // insert directive
-// var wrapper = document.createElement('div');
-// wrapper.setAttribute('ng-app', 'yahooTest');
-// wrapper.setAttribute('ng-controller', 'yahooTestController');
-//
-// var directive = document.createElement('div');
-// directive.setAttribute('yahoo', '');
-// wrapper.appendChild(directive);
-// document.body.appendChild(wrapper);
+// insert directive
+var wrapper = document.createElement('div');
+wrapper.setAttribute('ng-app', 'yahooTest');
+wrapper.setAttribute('ng-controller', 'yahooTestController');
 
-// Insert iframe with content from popup.html
-var iframe = document.createElement('iframe');
-iframe.src = chrome.extension.getURL("popup.html");
-iframe.align = 'right';
-iframe.width = '300';
-iframe.height = '100%';
-iframe.style.position = "absolute";
-iframe.style.top = '0px';
-iframe.style.right = '0px';
-iframe.style.zIndex = '10000000000';
-iframe.style.backgroundColor = 'white';
-document.body.appendChild(iframe);
+var directive = document.createElement('div');
+directive.setAttribute('yahoo', '');
+wrapper.appendChild(directive);
+document.body.appendChild(wrapper);
+
+// // Insert iframe with content from popup.html
+// var iframe = document.createElement('iframe');
+// iframe.src = chrome.extension.getURL("popup.html");
+// iframe.align = 'right';
+// iframe.width = '300';
+// iframe.height = '100%';
+// iframe.style.position = "absolute";
+// iframe.style.top = '0px';
+// iframe.style.right = '0px';
+// iframe.style.zIndex = '10000000000';
+// iframe.style.backgroundColor = 'white';
+// document.body.appendChild(iframe);
 
 
 // Find data from yahoo page
@@ -30,12 +30,12 @@ document.body.appendChild(iframe);
 
 
 // Send data to app
-setInterval(function() {
-  var data = document.querySelector('input').value;
-  console.log(data);
-  var w = document.querySelector('iframe').contentWindow;
-  w.postMessage({data: data}, '*');
-}, 2000);
+// setInterval(function() {
+//   var data = document.querySelector('input').value;
+//   console.log(data);
+//   var w = document.querySelector('iframe').contentWindow;
+//   w.postMessage({data: data}, '*');
+// }, 2000);
 
 // chrome.runtime.sendMessage({data: data}, function(response) {
 //   console.log(response.response);
